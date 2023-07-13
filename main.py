550fd0e4d781d230204733b8eefe7223323390bd
rahvlkvmar <90043137+rahvlkvmar@users.noreply.github.com>
2023-07-13 14:52:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def hello():
 
  <head>
 
-  <title>Infinite Project Generator</title>
+  <title>Personal AI Chef</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 
